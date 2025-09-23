3a76cbc2a1c5ed1920891036e26d56e25b0b2bdc
Alekhya <2300032926cseh1@gmail.com>
2025-09-23 15:00:53 +0530

    updated

--- backendcems/eventmanagement/src/main/java/com/example/eventmanagement/EventmanagementApplication.java
+++ backendcems/eventmanagement/src/main/java/com/example/eventmanagement/EventmanagementApplication.java
@@ -2,9 +2,10 @@ package com.example.eventmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- add this import
 
 @SpringBootApplication
-public class EventmanagementApplication extends SpringBootServletInitializer{ 
+public class EventmanagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EventmanagementApplication.class, args);
